88113d80443d2e97e89acde86a594782fb68c354
nika565 <115178529+nika565@users.noreply.github.com>
2023-04-24 14:23:29 -0300

    Update conect.php

--- php/conect.php
+++ php/conect.php
@@ -1,13 +1,8 @@
 <?php
-// $servidor = 'sql202.epizy.com';
-// $usuario = 'epiz_33943789';
-// $senha = 'tN3JLRmxF7wbXK';
-// $dbname = 'epiz_33943789_sparking';
-
-    $servidor = 'localhost';
-    $usuario = 'root';
-    $senha = '';
-    $dbname = 'sparking';
+$servidor = 'sparking-server';
+$usuario = 'csjlgxfhah';
+$senha = '4I57YSMN1D18O753$';
+$dbname = 'sparking-database';
 
     $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 ?>
